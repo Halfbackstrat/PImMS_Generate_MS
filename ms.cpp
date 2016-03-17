ecb8d41bbb221bf46deb07977af1ecaecd82e875
Halfbackstrat <mattbain17@gmail.com>
2016-03-17 21:53:14 +0000

    Added arguments

--- ms.cpp
+++ ms.cpp
@@ -1,18 +1,22 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int x, y, t, shot, bin;
 
 vector<int> ms(4096);
 
-int main()
+int main(int argc, char *argv[])
 {
-
+	if(argc < 2)
+	{
+	return 0;
+	}
 	ifstream myfile;
 
-	myfile.open("267_2colour_neutral.dat");
+	myfile.open(argv[1]);
 	if(myfile.is_open())
 	while (myfile >> x >> y >> t >> shot >> bin)
 	{
